26149988b9ccc1fd65cbb197bf069600fa5d7e26
Carsten Thue-Bludworth <cmt227@cornell.edu>
2014-06-24 14:43:01 -0400

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-/**** Javascript file for a game called... *****
-***** Author: Carsten Thue-Bludworth       *****
-***** Modified: June 23, 2014              ****/
-
-// Make the map container square with 100% height.
-var map= document.getElementById("map");
-var mapStyle= getComputedStyle(map);
-var mapHeight= mapStyle.getPropertyValue("height");
-map.style.width= mapHeight;
